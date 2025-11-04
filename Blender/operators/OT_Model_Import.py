5c9b8147de43a34f33ded5c3a28d77b93d341e6a
GreenTrafficLight <tailgunnerpursuit@gmail.com>
2025-11-05 00:30:49 +0100

    Changed to a directory importer

--- Blender/operators/OT_Model_Import.py
+++ Blender/operators/OT_Model_Import.py
@@ -16,35 +16,60 @@ from bpy_extras.io_utils import (
 from ...Utilities import *
 from ..utils.ImportModelRTuned import *
 
-class RTUNED_OT_Model_Import(Operator, ImportHelper):
-        """Load a R-Tuned Ultimate Street Racing model file"""
-        bl_idname = "import_rtuned.data"
-        bl_label = "Import R-Tuned Ultimate Street Racing model"
+class RTUNED_OT_Model_Import(bpy.types.Operator):
+    """Load R-Tuned folder"""
+    bl_idname = "import_rtuned.data"
+    bl_label = "Import R-Tuned Ultimate Street Racing folder"
+    bl_options = {'REGISTER', 'UNDO'}
 
-        filename_ext = ""
-        filter_glob: StringProperty(default="*", options={'HIDDEN'}, maxlen=255,)
+    directory: StringProperty(
+        name="Directory",
+        description="Select folder containing geometry.bin + object.bin",
+        subtype='DIR_PATH'
+    )
 
-        clear_scene: BoolProperty(
-            name="Clear scene",
-            description="Clear the scene",
-            default=False,
-        )
+    clear_scene: BoolProperty(
+        name="Clear scene",
+        default=False,
+    )
 
-        def execute(self, context):   
-            importModel(self.filepath, self.clear_scene)
+    def invoke(self, context, event):
+        context.window_manager.fileselect_add(self) 
+        return {'RUNNING_MODAL'}
 
-            return {'FINISHED'}
+    def execute(self, context):
+        geometry_path = os.path.join(self.directory, "geometry.bin")
+        if not os.path.exists(geometry_path):
+            self.report({'ERROR'}, "geometry.bin not found in folder")
+            return {'CANCELLED'}
         
-def importModel(filepath: str, clear_scene: bool):
+        object_path = os.path.join(self.directory, "object.bin")
+        if not os.path.exists(object_path):
+            self.report({'ERROR'}, "object.bin not found in folder")
+            return {'CANCELLED'}
+
+        importModel(self.directory, self.clear_scene)
+        return {'FINISHED'}
+        
+def importModel(directory: str, clear_scene: bool):
     if clear_scene:
         clearScene()
 
-    file = open(filepath, 'rb')
-    filename =  filepath.split("\\")[-1]
-    br = BinaryReader(file)
+    geometry_path = os.path.join(directory, "geometry.bin")
+    geometry_file = open(geometry_path, "rb")
+    
+    object_path  = os.path.join(directory, "object.bin")
+    object_file   = open(object_path, "rb")
 
-    bin = BIN()
-    bin.read(br)
-    build_bin(bin, filename)
+    br_geometry = BinaryReader(geometry_file)
+    bin_geometry = GEOMETRY()
+    bin_geometry.read(br_geometry)
+
+    br_object = BinaryReader(object_file)
+    bin_object = OBJECT()
+    bin_object.read(br_object)
+
+    folder_name = os.path.basename(os.path.normpath(directory))
+    build_bin(bin_geometry, bin_object, folder_name)
 
     return {'FINISHED'}
